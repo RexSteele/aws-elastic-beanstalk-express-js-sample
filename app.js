6979ebb57058fa2ec8f7f512ad9ddb3a7a26e300
RexSteele <rexralphsteele@gmail.com>
2021-05-27 14:51:32 -0600

    Another minor change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello my honey, hello my darling, hello my ragtime gal!'));
+app.get('/', (req, res) => res.send('Here we go, one more time'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
